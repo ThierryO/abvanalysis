0de30887161273d0c0e96d98fbb9a34d978c872d
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-09-19 20:35:46 +0200

    bugfixes from R CMD Check

--- R/prepare_analysis_composite.R
+++ R/prepare_analysis_composite.R
@@ -2,9 +2,11 @@
 #' @inheritParams prepare_analysis_dataset
 #' @inheritParams prepare_analysis_lrt
 #' @inheritParams prepare_dataset
+#' @param dataset a dataframe with the metadata of the analysis
 #' @export
 #' @importFrom n2kanalysis n2k_composite
 #' @importFrom assertthat assert_that is.count
+#' @importFrom dplyr %>% add_rownames mutate_ select_ arrange_ filter_
 prepare_analysis_composite <- function(dataset, raw.connection, analysis.path){
   analysis.path <- check_path(paste0(analysis.path, "/"), type = "directory")
   check_dataframe_variable(
